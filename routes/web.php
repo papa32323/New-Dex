aaa587c7a9f0ab6f0ce7aee6550099efc6249e0d
Thair Bazán <thairbazan07@gmail.com>
2025-11-24 16:58:09 -0500

    Segundo Commit

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,17 @@ use Illuminate\Support\Facades\Http;
 
 Route::get('/', function () {
 
-    
     $respuesta = Http::get('https://pokeapi.co/api/v2/pokemon?limit=20');
+
+    // Si la API falla, evita error 500
+    if ($respuesta->failed()) {
+        return "Error al obtener datos de la PokeAPI";
+    }
 
     $data = $respuesta->json();
 
-    
-    $pokemones = $data['results']; 
+    // Si no existe "results", evitar error 500
+    $pokemones = $data['results'] ?? [];
 
-   
     return view('welcome', compact('pokemones'));
 });
